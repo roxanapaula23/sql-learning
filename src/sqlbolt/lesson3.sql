708ca9ba0d9618b1b1431c196fb78ecfbca49441
roxanapaula23 <roxanasfabu@gmail.com>
2022-10-04 18:10:42 +0300

    Lesson 3

---
+++ src/sqlbolt/lesson3.sql
@@ -0,0 +1,8 @@
+SELECT * FROM movies
+WHERE title LIKE "Toy%Story%"; 
+SELECT * FROM movies
+WHERE director LIKE "John%Lasseter"; 
+SELECT * FROM movies
+WHERE director NOT LIKE "John%Lasseter"; 
+SELECT * FROM movies
+WHERE title LIKE "WALL%"
