ddb2b79ec1195926a406c4ee53c49aeb0460459f
roxanapaula23 <roxanasfabu@gmail.com>
2022-10-07 19:33:35 +0300

    Lesson 5

---
+++ src/sqlbolt/lesson5.sql
@@ -0,0 +1,14 @@
+SELECT city, population FROM north_american_cities
+WHERE country = "Canada";
+SELECT city, latitude FROM north_american_cities
+WHERE country = "United States"
+ORDER BY latitude DESC;
+SELECT city, latitude FROM north_american_cities
+WHERE country = "United States"
+ORDER BY latitude DESC;
+SELECT city, latitude FROM north_american_cities
+WHERE country = "United States"
+ORDER BY latitude DESC;
+SELECT city, latitude FROM north_american_cities
+WHERE country = "United States"
+ORDER BY latitude DESC;
