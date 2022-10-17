5cef12c96e360796f4971d37d804e4444a2f3184
roxanapaula23 <roxanasfabu@gmail.com>
2022-10-18 00:04:37 +0300

    Lesson14

---
+++ src/sqlbolt/lesson14.sql
@@ -0,0 +1,9 @@
+UPDATE movies
+SET director = "John Lasseter"
+WHERE id = 2;
+UPDATE movies
+SET year = 1999
+WHERE id = 3;
+UPDATE movies
+SET title = "Toy Story 3", director = "Lee Unkrich"
+WHERE id = 11;
