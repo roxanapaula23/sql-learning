a58fb54a712bab44552dde8f63af374d8d43eef5
roxanapaula23 <roxanasfabu@gmail.com>
2022-10-17 23:35:20 +0300

    Lesson 12

---
+++ src/sqlbolt/lesson12.sql
@@ -0,0 +1,8 @@
+SELECT director, COUNT(id) AS Number_movies_directed
+FROM movies
+GROUP BY director;
+SELECT director, SUM(domestic_sales + international_sales) as Cumulative_sales_from_all_movies
+FROM movies
+INNER JOIN boxoffice
+ON movies.id = boxoffice.movie_id
+GROUP BY director;
---
+++ src/sqlbolt/lesson13.sql
@@ -0,0 +1,4 @@
+INSERT INTO movies 
+VALUES (4, "Toy Story 4", "Brad Lasse", 2010, 91);
+INSERT INTO boxoffice 
+VALUES (4, 8.7, 340000000, 270000000);
